478573eeed1a5528e4f67ba9d29978247f073d4d
MercyMilkahNjokiKahungu <mercy.njoki@student.moringaschool.com>
2022-08-14 16:15:43 +0300

    sign in/up form

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 
-document.querySelector("body").appendChild(h2);
- const pass_field = document.querySelector(".pass-key");
- const showBtn = document.querySelector(".show");
- showBtn.addEventListener("click", function() {
-    if(pass_field.type === "password"){
-        pass_field.type = "text";
-        showBtn.textContent = "HIDE";
-        showBtn.style.color = "#3498db";
-    }
-    else{
-        pass_field.type ="password";
-        showBtn.textContent ="SHOW";
-        showBtn.style.color = "black";
-    }
- });
+// document.querySelector("body").appendChild(h2);
+//  const pass_field = document.querySelector(".pass-key");
+//  const showBtn = document.querySelector(".show");
+//  showBtn.addEventListener("click", function() {
+//     if(pass_field.type === "password"){
+//         pass_field.type = "text";
+//         showBtn.textContent = "HIDE";
+//         showBtn.style.color = "#3498db";
+//     }
+//     else{
+//         pass_field.type ="password";
+//         showBtn.textContent ="SHOW";
+//         showBtn.style.color = "black";
+//     }
+//  });
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
